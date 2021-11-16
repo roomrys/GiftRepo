b7fa0d529f9f0bbd0540d547364c06986c9e53d8
roomrys <38435167+roomrys@users.noreply.github.com>
2021-11-16 09:14:13 -0800

    added ejs template
    serving locally using ejs template: "<%= userid %>'s Xmas List" with path http://localhost:3000/:userid

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 3000;
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.set('view engine', 'ejs');
+app.get('/:userid', function(req, res) {
+    res.render('index', {data : {userid: req.params.userid}});
 });
 
 app.listen(port, () => {
